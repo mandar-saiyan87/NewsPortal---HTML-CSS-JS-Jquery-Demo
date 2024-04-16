6b5743163e22b841667641bf064977e07f417566
mandar-saiyan87 <mdman2257@gmail.com>
2024-04-16 17:25:20 +0530

    api created

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors')
+
+const app = express();
+const PORT = 3000;
+app.use(express.json())
+app.use(cors())
+const API_KEY = 'f096911f2b4a49a1b9a770322586cb15';
+const NEWS_API_URL = 'https://newsapi.org/v2/everything';
+
+// Route to fetch data from NewAPI
+app.get('/news/:query', async (req, res) => {
+  try {
+    const query = req.params.query;
+    const response = await axios.get(`${NEWS_API_URL}?q=${query}&apiKey=${API_KEY}`);
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching news:', error);
+    res.status(500).json({ error: 'Failed to fetch news' });
+  }
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
