e9f6634e4d3acb7eb3ca1ef89c161afefdb4d941
mandar-saiyan87 <mdman2257@gmail.com>
2024-04-16 16:52:45 +0530

    newstags changed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,19 +11,19 @@ const newsdata = [
   {
     id: 1,
     title: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, dolor? Tempora voluptatibus officiis itaque ad dolorem libero optio dignissimos sit.",
-    tag: "Economy",
+    tag: "CNBC",
     image: 'https://www.sociallifeproject.org/content/images/2023/10/IMG_1391.jpeg'
   },
   {
     id: 2,
     title: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, dolor? Tempora voluptatibus officiis itaque ad dolorem libero optio dignissimos sit.",
-    tag: "Science & Tech",
+    tag: "Techcrunch",
     image: "https://upscpathshala.com/content/wp-content/uploads/2020/10/Best-Books-for-Science-and-Technology-for-UPSC.jpg"
   },
   {
     id: 3,
     title: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, dolor? Tempora voluptatibus officiis itaque ad dolorem libero optio dignissimos sit.",
-    tag: "Sports",
+    tag: "ESPN",
     image: "https://cdn.evolve-mma.com/wp-content/uploads/2021/04/GSP-training-at-Evolve-MMA.jpg"
   }
 ]
@@ -71,7 +71,7 @@ $(document).ready(function () {
 
   setTimeout(() => {
     $('.loading-spinner').css('display', 'none')
-  }, 800);
+  }, 600);
 
   // To open sidemenu bar for mobile & tablets
   $(".hbmenu").on('click', function () {
@@ -112,7 +112,7 @@ $(document).ready(function () {
         `
         newssearch.append(searched)
       });
-    } else { 
+    } else {
       newssearch.html('<p>No news found for the topic</p>')
     }
   })
