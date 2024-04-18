c26be3f96d065a2bd0ded69c66790152a8b93033
mandar-saiyan87 <mdman2257@gmail.com>
2024-04-18 09:23:53 +0530

    localhost changed

--- api/index.js
+++ api/index.js
@@ -9,9 +9,12 @@ app.use(express.json())
 app.use(cors())
 const API_KEY = process.env.API_KEY;
 const NEWS_API_URL = 'https://newsapi.org/v2/everything';
+const today = new Date()
+const sevendays = today.setDate(today.getDate() - 2);
 
 // Route to fetch data from NewAPI
 app.get('/news/:query', async (req, res) => {
+  console.log(sevendays)
   try {
     const query = req.params.query;
     const response = await axios.get(`${NEWS_API_URL}?q=${query}&language=en&apiKey=${API_KEY}`);
@@ -25,5 +28,5 @@ app.get('/news/:query', async (req, res) => {
 
 // Start Server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://loaclhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
